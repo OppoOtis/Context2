cada40bb7f11968e4b3c3a93737c207dee749bfb
OppoOtis <joppedek2003@gmail.com>
2023-02-07 23:46:50 +0100

    Basic Player Movement

--- Context2/Assets/Scripts/Player/PlayerController.cs
+++ Context2/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    private Vector2 move;
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        move = context.ReadValue<Vector2>();
+    }
+    private void Update()
+    {
+        MovePlayer();
+    }
+    public void MovePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
