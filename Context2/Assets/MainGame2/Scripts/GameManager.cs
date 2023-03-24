b661b3a5a330af05db9c1febe39715ec3806ad6a
OppoOtis <joppedek2003@gmail.com>
2023-03-24 17:09:20 +0100

    new sphhu

--- Context2/Assets/MainGame2/Scripts/GameManager.cs
+++ Context2/Assets/MainGame2/Scripts/GameManager.cs
@@ -234,7 +234,9 @@ public class GameManager : MonoBehaviourPunCallbacks
                 if (roundSection == 0)
                 {
                     Round1Scenarios(false);
+                    Debug.Log("test");
                     triggerShuffle = true;
+                    waitForPlayers.SetActive(false);
                     foreach (GameObject obj in roundOBJ[0].objects)
                     {
                         obj.SetActive(false);
