ef307ff8c8c94f56ab8bda0106cccc3dcdf761dc
OppoOtis <joppedek2003@gmail.com>
2023-03-30 00:06:10 +0200

    Revert "nog meer"
    This reverts commit 4654121d44eab176d897f778c33cafc5757934eb.

--- Context2/Assets/MainGame2/Scripts/GameManager.cs
+++ Context2/Assets/MainGame2/Scripts/GameManager.cs
@@ -234,10 +234,6 @@ public class GameManager : MonoBehaviourPunCallbacks
                     {
                         OnClickNextSection();
                     }
-                    else
-                    {
-                        timer = 0;
-                    }
                 }
             }
             else
